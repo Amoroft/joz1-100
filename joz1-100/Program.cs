b99433c2422dc3b11e9663f63edc9ac74c010e1e
salehnavab <salehnavabm@gmail.com>
2023-10-19 00:31:22 +0330

    Add project files.

---
+++ joz1-100/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Even numbers between 100 and 1000:");
+
+        for (int i = 100; i <= 1000; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
